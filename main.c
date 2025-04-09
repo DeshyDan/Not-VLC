31c0d68359f3b149a01c95c541820c0f4f13ff9b
Deshy Dan <devdeshy@gmail.com>
2025-04-09 18:51:34 +0200

    Add test mp4 container

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include "decoders/video_decoder.h"
+#include "libs/microlog/microlog.h"
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    const char *URL = "../data/videos/test.mp4";
+
+    VideoDecoder *decoder = video_decoder_init(URL);
+    if (!decoder) {
+        log_error("Could not initialize video decoder");
+        return -1;
+    }
+    // Dump format information
+    av_dump_format(decoder->fmt_ctx, 0, URL, 0);
+
+    // Clean up
+    video_decoder_destroy(decoder);
 }
